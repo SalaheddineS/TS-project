9b67344da2ff21732bc63dee3145fcaeb7a98df7
salaheddine <salaheternal66@gmail.com>
2023-01-31 20:32:20 +0100

    Start of the Project 🎉

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { connect } from "mongoose";
 import { config } from "dotenv";
+import UserRouter from "./Routers/UserRouter";
+
 config(); // Load .env file
 if (!process.env.MONGO_URL) throw new Error("MONGO_URL is not defined");
 if (!process.env.PORT) throw new Error("PORT is not defined");
@@ -8,6 +10,9 @@ if (!process.env.DEFAULT_IMAGE) throw new Error("DEFAULT_IMAGE is not defined");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/users", UserRouter);
+
 connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(process.env.PORT);
