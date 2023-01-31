a74b7ebf76afb7ccf9cae70a8902382b7d1458be
salaheddine <salaheternal66@gmail.com>
2023-01-31 22:38:16 +0100

    Advanced a bit on the project

---
+++ src/Controllers/AuthController.ts
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import { Request, Response } from "express";
+import User from "../Models/User";
+import bcrypt from "bcrypt";
+
+export const login = async (req: Request, res: Response) => {
+  const current = req.body;
+  if (!current.email || !current.password)
+    return res.status(400).json({ message: "Email and password are required" });
+  const currentUserInDB = await User.findOne({ email: current.email });
+  if (!currentUserInDB)
+    return res.status(400).json({ message: "Email or password are incorrect" });
+  const isPasswordCorrect = await bcrypt.compare(
+    current.password,
+    currentUserInDB.password
+  );
+  if (!isPasswordCorrect)
+    return res.status(400).json({ message: "Email or password are incorrect" });
+  if (!process.env.JWT_SECRET) throw new Error("JWT_SECRET is not defined");
+  const token = jwt.sign(
+    { id: currentUserInDB._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" },
+    (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    }
+  );
+  res.json("Successfully logged in");
+};
+
+export const logout = async (req: Request, res: Response) => {
+  res.json("Successfully logged out");
+};
--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,18 @@ import express from "express";
 import { connect } from "mongoose";
 import { config } from "dotenv";
 import UserRouter from "./Routers/UserRouter";
+import AuthRouter from "./Routers/AuthRouter";
 
 config(); // Load .env file
 if (!process.env.MONGO_URL) throw new Error("MONGO_URL is not defined");
 if (!process.env.PORT) throw new Error("PORT is not defined");
 if (!process.env.DEFAULT_IMAGE) throw new Error("DEFAULT_IMAGE is not defined");
-
+if(!process.env.JWT_SECRET) throw new Error("JWT_SECRET is not defined");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", UserRouter);
-
+app.use("/api/auth", AuthRouter);
 connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(process.env.PORT);
